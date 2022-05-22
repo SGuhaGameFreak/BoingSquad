e21fa91e2494aa1a5549103cf3eb6d12e952c333
freakNok <shounok.guha@gmail.com>
2022-05-22 11:41:22 +0530

    Initial commit

---
+++ src/main/java/com/freaknok/boingsquad/BoingSquad.java
@@ -0,0 +1,35 @@
+package com.freaknok.boingsquad;
+
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(BoingSquad.MOD_ID)
+public class BoingSquad
+{
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "boingsquad";
+
+    public BoingSquad() {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+}
